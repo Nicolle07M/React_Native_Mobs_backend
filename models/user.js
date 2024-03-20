a64fad88ab174f4cf0cf5bfb20a0852608e3009b
Nicolle07M <marqueznicolle421@gmail.com>
2024-03-20 10:25:18 -0500

    feat(backend): add encryption and check password encryption

--- models/user.js
+++ models/user.js
@@ -1,54 +1,45 @@
 const db = require('../config/config');
+const bcrypt = require('bcryptjs');
+
+
 const User = {};
-
-
-User.create = (user, result) => {
-
-    const sql = `
-        INSERT INTO
-            users(
-                email,
-                name,
-                lastname,
-                phone,
-                image,
-                password,
-                created_at,
-                updated_at
+User.create = async (user, result) => {
+        const hash = await bcrypt.hash(user.password, 10);
+        const sql =
+            `INSERT INTO users (
+            email,
+            name,
+            lastname,
+            phone,
+            image,
+            password,
+            created_at,
+            updated_at
             )
-        VALUES(?, ?, ?, ?, ?, ?, ?, ?)
-    `;
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
 
-    db.query
-    (
-        sql,
-
-        [
-            user.email,
-            user.name,
-            user.lastname,
-            user.phone,
-            user.image,
-            user.password,
-            new Date(),
-            new Date()
-        ],
-
-        (err, res) => {
-            if (err) {
-                console.log('Error:', err);
-                result(err, null);
+        db.query(sql,
+            [
+                user.email,
+                user.name,
+                user.lastname,
+                user.phone,
+                user.image,
+                hash,
+                new Date(),
+                new Date()
+            ], (err, res) => {
+                if (err) {
+                    console.log('Error al crear el usuario: ', err);
+                    result(err, null);
+                }
+                else {
+                console.log('Usuario creado: ', { id: res.insertId, ...user
+});
+                result(null, { id: res.insertId, ...user });
             }
-            else {
-                console.log('Id del nuevo usuario:', res.insertId);
-                result(null, res.insertId);
-
-            }
-
         }
-
-    )
+    );
 }
 
- 
-module.exports = User;
+module.exports = User; 
